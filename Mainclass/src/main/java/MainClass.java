7d29069a43c81f5e838f0ad73b03e4fcba28e50b
knoldus <chiranjeev.kumar@knoldus.com>
2021-11-30 18:58:40 +0530

    first commit

---
+++ Mainclass/src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Additon of 2 and 4 = "+ImplementationOfMain.Addition(2,4));
+        System.out.println("Subtraction of 4 and 2 = "+ImplementationOfMain.Subtraction(4,2));
+        System.out.println("Multiplication of 3 and 4 = "+ImplementationOfMain.Multiplicatio(3,4));
+        System.out.println("Division of 10 and 5 = "+ImplementationOfMain.Division(10,5));
+    }
+}
---
+++ implementation/src/main/java/ImplementationOfMain.java
@@ -0,0 +1,18 @@
+public class ImplementationOfMain {
+   public static int Addition(int number1, int number2)
+    {
+        return number1+number2;
+    }
+    public static int Subtraction(int number1, int number2)
+    {
+        return number1-number2;
+    }
+    public static int Multiplicatio(int number1, int number2)
+    {
+        return number1*number2;
+    }
+    public static int Division(int number1, int number2)
+    {
+        return number1/number2;
+    }
+}
